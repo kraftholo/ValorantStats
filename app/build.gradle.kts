94d3405a0fcec2b374d00c9ff4baa69dd6e5a7d1
Shobhit <shobhitk007@gmail.com>
2021-09-02 18:08:00 +0530

    - Addition of buildSrc directory - Defining dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,16 @@ plugins {
 }
 
 android {
-    compileSdk = 30
-    buildToolsVersion = "30.0.3"
+    compileSdk = Android.compileSdk
+    buildToolsVersion = Android.buildTools
 
     defaultConfig {
-        applicationId = "com.sk.valorantstats"
-        minSdk = 21
-        targetSdk = 30
-        versionCode = 1
-        versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        applicationId = Android.appId
+        minSdk = Android.minSdk
+        targetSdk = Android.targetSdk
+        versionCode = Android.versionCode
+        versionName = Android.versionName
+        testInstrumentationRunner = AndroidXTest.runner
     }
 
     buildTypes {
@@ -30,21 +30,21 @@ android {
         viewBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0"
+        kotlinCompilerExtensionVersion = Compose.composeVersion
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation(AndroidX.coreKtx)
+    implementation(AndroidX.appCompat)
 
-    implementation("androidx.activity:activity-compose:1.3.0")
+    implementation(Compose.activity)
+    implementation(Compose.ui)
+    implementation(Compose.material)
+    implementation(Compose.tooling)
+    implementation(Compose.navigation)
+    implementation(Compose.hiltNavigation)
 
-    implementation("androidx.compose.foundation:foundation:1.0.0")
-    implementation("androidx.compose.foundation:foundation-layout:1.0.0")
-    implementation("androidx.compose.material:material:1.0.0")
-    implementation("androidx.compose.runtime:runtime:1.0.0")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0")
+    implementation(Google.material)
 
 }
---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,18 @@
+object Modules {
+
+    const val app = ":app"
+
+    const val core = ":core"
+
+    const val components = ":components"
+
+    const val constants = ":constants"
+
+    const val weapon = ":weapon"
+    const val weaponDataSource = ":weapon:weapon-datasource"
+    const val weaponDataSourceTest = ":weapon:weapon-datasource-test"
+    const val weaponDomain = ":weapon:weapon-domain"
+    const val weaponInteractors = ":weapon:weapon-interactors"
+    const val ui_weaponDetail = ":weapon:ui-weaponDetail"
+    const val ui_weaponList = ":weapon:ui-weaponList"
+}
