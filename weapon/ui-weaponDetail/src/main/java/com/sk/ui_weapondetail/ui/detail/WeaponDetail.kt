400e09d6bbd66b97ff117697682a02a8f2fd83b2
shobhit <shobhitkotnala97@gmail.com>
2021-11-09 15:33:26 +0530

    Ui Additions

--- weapon/ui-weaponDetail/src/main/java/com/sk/ui_weapondetail/ui/detail/WeaponDetail.kt
+++ weapon/ui-weaponDetail/src/main/java/com/sk/ui_weapondetail/ui/detail/WeaponDetail.kt
@@ -1,6 +1,5 @@
 package com.sk.ui_weapondetail.ui.detail
 
-import android.net.Uri
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
@@ -8,32 +7,18 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import coil.ImageLoader
 import coil.compose.rememberImagePainter
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
 import com.sk.core.domain.ProgressBarState
+import com.sk.ui_weapondetail.R
 import com.sk.ui_weapondetail.ui.composable.ChromaDisplay
-import com.sk.ui_weaponlist.R
+
 import com.sk.weapon_domain.Weapon
-import com.sk.weapon_domain.skin.Chroma
-import com.sk.weapon_domain.skin.Level
 import com.sk.weapon_domain.skin.Skin
-import java.lang.Math.abs
 
 @Composable
 fun WeaponDetail(
@@ -82,37 +67,80 @@ fun WeaponDetail(
                                 verticalAlignment = Alignment.CenterVertically,
                                 horizontalArrangement = Arrangement.Start
                             ) {
-
-                                Text(
+                                Row(
                                     modifier = Modifier
                                         .align(Alignment.CenterVertically)
-                                        .padding(end = 8.dp),
-                                    text = weapon.displayName,
-                                    style = MaterialTheme.typography.h1
-                                )
-                                val iconPainter = rememberImagePainter(
-                                    weapon.killStreamIcon,
-                                    imageLoader = imageLoader,
-                                    builder = {
-                                        placeholder(if (isSystemInDarkTheme()) R.drawable.black_background else R.drawable.white_background)
-                                    }
+                                        .weight(6f)
+                                ) {
+                                    Text(
+                                        modifier = Modifier
+                                            .padding(end = 8.dp),
+                                        text = weapon.displayName,
+                                        style = MaterialTheme.typography.h1
+                                    )
+                                    val iconPainter = rememberImagePainter(
+                                        weapon.killStreamIcon,
+                                        imageLoader = imageLoader,
+                                        builder = {
+                                            placeholder(if (isSystemInDarkTheme()) R.drawable.black_background else R.drawable.white_background)
+                                        }
+                                    )
+
+                                    Image(
+                                        modifier = Modifier
+                                            .height(40.dp)
+                                            .width(40.dp),
+                                        painter = iconPainter,
+                                        contentDescription = weapon.displayName,
+                                        contentScale = ContentScale.Fit
+                                    )
+                                }
+
+                                weapon.shopData?.let {
+                                    Text(
+                                        modifier = Modifier
+                                            .align(Alignment.CenterVertically)
+                                            .weight(2f),
+                                        text = it.cost.toString(),
+                                        style = MaterialTheme.typography.h2
+                                    )
+                                }
+                            }
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceAround,
+                                verticalAlignment = Alignment.CenterVertically
+                            ){
+                                Text(
+                                    modifier = Modifier.padding(bottom = 4.dp,start = 4.dp),
+                                    text = weapon.category.uiValue,
+                                    style = MaterialTheme.typography.subtitle1
                                 )
 
-                                Image(
-                                    modifier = Modifier
-                                        .height(40.dp)
-                                        .width(40.dp)
-                                        .align(Alignment.CenterVertically),
-                                    painter = iconPainter,
-                                    contentDescription = weapon.displayName,
-                                    contentScale = ContentScale.Fit
+                                Text(
+                                    modifier = Modifier.padding(bottom = 4.dp, start = 8.dp),
+                                    text = weapon.weaponStats?.fireMode?.uiValue ?: "Unknown",
+                                    style = MaterialTheme.typography.subtitle1
                                 )
                             }
-                            Text(
-                                modifier = Modifier.padding(bottom = 4.dp),
-                                text = weapon.category.uiValue,
-                                style = MaterialTheme.typography.subtitle1
-                            )
+
+                            weapon.weaponStats?.let { weaponStats ->
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Start,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Icon(
+                                        painterResource(id = R.drawable.wall_penetration),
+                                        "wall_penetration"
+                                    )
+                                    Text(
+                                        modifier = Modifier.padding(bottom = 4.dp, start = 2.dp),
+                                        text = weaponStats.wallPenetration?.uiValue ?: "Unknown",
+                                        style = MaterialTheme.typography.subtitle1
+                                    )
+                                }
+                            }
+
                         }
                     }
                     state.skins?.forEach {
@@ -153,14 +181,19 @@ fun WeaponSkinCollection(
     imageLoader: ImageLoader,
     onSelectSkin: (String) -> Unit
 ) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        text = skin.displayName
-    )
-    ChromaDisplay(chromaList = skin.chromas, imageLoader = imageLoader)
-    if (skin.hasLevels) {
-        Button(onClick = { onSelectSkin(skin.uuid) }) {
-            Text("Show Skin Levels")
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier.padding(4.dp)
+    ){
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = skin.displayName
+        )
+        ChromaDisplay(chromaList = skin.chromas, imageLoader = imageLoader)
+        if (skin.hasLevels) {
+            Button(onClick = { onSelectSkin(skin.uuid) }) {
+                Text("Show Skin Levels")
+            }
         }
     }
 }
